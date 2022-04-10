f6674fa8d4571abbb46f0c2eebadc52a316ff4e2
Dirk Eddelbuettel <edd@debian.org>
2022-04-10 17:52:50 -0500

    initial (incomplete) commit

---
+++ R/controlFiles.R
@@ -0,0 +1,257 @@
+
+.addDepends <- function(dt, con) {
+    if (is.na(dt[,Depends])) return(invisible(NULL))
+    dep <- dt[,Depends]
+    dep <- gsub("R \\(.*\\)(, )+", "", dep, perl=TRUE)
+    if (nchar(dep) == 0) return(invisible(NULL))
+    deps <- strsplit(dep, ",")[[1]]
+    for (i in deps) {
+        i <- gsub("^ ", "", i)
+        if (i %in% c("utils", "methods", "stats")) next
+        cat(", r-cran-", tolower(i), sep="", file=con, append=TRUE)
+    }
+}
+
+.addImports <- function(dt, con) {
+    if (is.na(dt[,Imports])) return(invisible(NULL))
+    imp <- dt[,Imports]
+    imps <- strsplit(imp, ",")[[1]]
+    for (i in imps) {
+        i <- gsub("^ ", "", i)
+        if (i %in% c("utils", "methods", "stats")) next
+        cat(", r-cran-", tolower(i), sep="", file=con, append=TRUE)
+    }
+}
+
+.addLinkingTo <- function(dt, con) {
+    if (is.na(dt[,LinkingTo])) return(invisible(NULL))
+    lto <- dt[,LinkingTo]
+    ltos <- strsplit(lto, ",")[[1]]
+    for (i in ltos) {
+        i <- gsub("^ ", "", i)
+        i <- gsub("\\n", "", i)
+        if ("Rcpp" == i && grepl("Rcpp", dt[,Imports])) next 	# already covered
+        cat(", r-cran-", tolower(i), sep="", file=con, append=TRUE)
+    }
+}
+
+.addSuggests <- function(dt, con) {
+    if (is.na(dt[,Suggests])) return(invisible(NULL))
+    sgg <- dt[,Suggests]
+    sggs <- strsplit(sgg, ",")[[1]]
+    first <- TRUE
+    for (i in sggs) {
+        i <- gsub("^ ", "", i)
+        i <- gsub("\\n", "", i)
+        if (!first) cat(", ")
+        cat("r-cran-", tolower(i), sep="", file=con, append=TRUE)
+        first <- FALSE
+    }
+}
+
+.hasConfigField <- function(key) {
+    is.finite(match(key, names(.pkgenv)))
+}
+
+.getConfig <- function(key) {
+    stopifnot(`key is not present`=is.finite(match(key, names(.pkgenv))))
+    .pkgenv[[key]]
+}
+
+writeControl <- function(pkg, db, repo=c("CRAN", "Bioc"), debug=FALSE) {
+    if (missing(db)) db <- .pkgenv[["db"]]
+    stopifnot(`db must be data.frame`=inherits(db, "data.frame"))
+    repo <- tolower(match.arg(repo))
+    if (!inherits(db, "data.table")) setDT(db)
+    ind <- match(pkg, db[,Package])
+    if (is.na(ind)) stop("Package '", pkg, "' not known to package database.", call. = FALSE)
+
+    ## todo: check license and all that
+    D <- db[ind,]
+    if (debug) print(D)
+
+    lp <- tolower(pkg)
+    maint <- .getConfig("maintainer")
+    dhcompat <- .getConfig("debhelper_compat")
+    rdevver <- .getConfig("minimum_r_version")
+    stdver <- .getConfig("debian_policy_version")
+
+    binary <- D[,NeedsCompilation] == "yes"
+
+    con <- file("debian.control", "wt")
+    cat("Source: r-", repo, "-", lp, "\n",
+        "Section: gnu-r\n",
+        "Priority: optional\n",
+        "Maintainer: ", maint, "\n",
+        "Build-Depends: debhelper-compat (= ", dhcompat, "), r-base-dev (>= ", rdevver, "), dh-r",
+        sep="", file=con)
+    .addDepends(D, con)
+    .addImports(D, con)
+    .addLinkingTo(D, con)
+    cat("\nStandards-Version: ", stdver, "\n",
+        "Homepage: https://cran.r-project.org/package=", pkg, "\n\n",
+        "Package: r-", repo, "-", lp, "\n",
+        "Architecture: ", if (binary) "any" else "all", "\n",
+        "Depends: ", if (binary) "${shlibs:Depends}, " else "", "${misc:Depends}, ${R:Depends}",
+        sep="", file=con)
+    .addDepends(D, con)
+    .addImports(D, con)
+    .addLinkingTo(D, con)
+    cat("\nSuggests: ", file=con)
+    .addSuggests(D, con)
+    cat("\nDescription: CRAN Package '", pkg, "' (", D[,Title], ")\n ", D[,Description],
+        sep="", file=con)
+    cat("\n", file=con)
+    close(con)
+}
+
+writeDsc <- function(pkg, db, repo=c("CRAN", "Bioc"), debug=FALSE) {
+    if (missing(db)) db <- .pkgenv[["db"]]
+    stopifnot(`db must be data.frame`=inherits(db, "data.frame"))
+    repo <- tolower(match.arg(repo))
+    if (!inherits(db, "data.table")) setDT(db)
+    ind <- match(pkg, db[,Package])
+    if (is.na(ind)) stop("Package '", pkg, "' not known to package database.", call. = FALSE)
+
+    ## todo: check license and all that
+    D <- db[ind,]
+    if (debug) print(D)
+
+    srcfile <- paste0(pkg, "_", D[,Version], ".tar.gz")
+    targz <- file.path("/home/edd/cranberries/sources/", srcfile)  ## TODO: archive
+    stopifnot(file.exists(targz))
+
+    lp <- tolower(pkg)
+    maint <- .getConfig("maintainer")
+    dhcompat <- .getConfig("debhelper_compat")
+    rdevver <- .getConfig("minimum_r_version")
+    stdver <- .getConfig("debian_policy_version")
+
+    binary <- D[,NeedsCompilation] == "yes"
+
+    nam <- paste0("r-", repo, "-", lp)
+    con <- file(paste0(nam, ".dsc"), "wt")
+    cat("Format: 1.0\n",
+        "Source: ", nam, "\n",
+        "Version: ", D[,Version], "-1.obs.1\n",
+        "Binary: r-", repo, "-", lp, "\n",
+        "Maintainer: ", maint, "\n",
+        "Architecture: ", if (binary) "any" else "all", "\n",
+        "Homepage: https://cran.r-project.org/package=", pkg, "\n",
+        "Standards-Version: ", stdver, "\n",
+        "Build-Depends: debhelper-compat (= ", dhcompat, "), r-base-dev (>= ", rdevver, "), dh-r",
+        sep="", file=con)
+    .addDepends(D, con)
+    .addImports(D, con)
+    .addLinkingTo(D, con)
+    sz <- file.info(targz)$size
+    md5sum <- system(paste("md5sum", targz, "| cut -f1 -d' '"), intern=TRUE)
+    sha1sum <- system(paste("sha1sum", targz, "| cut -f1 -d' '"), intern=TRUE)
+    sha256sum <- system(paste("sha256sum", targz, "| cut -f1 -d' '"), intern=TRUE)
+    cat("\nFiles:\n ", md5sum, " ", sz, " ", srcfile, "\n",
+        "CheckSums-Sha1:\n ", sha1sum, " ", sz, " ", srcfile, "\n",
+        "CheckSums-Sha256:\n ", sha256sum, " ", sz, " ", srcfile, "\n",
+        sep="", file=con, append=TRUE)
+    close(con)
+}
+
+writeChangelog <- function(pkg, db, repo=c("CRAN", "Bioc"), debug=FALSE) {
+    if (missing(db)) db <- .pkgenv[["db"]]
+    stopifnot(`db must be data.frame`=inherits(db, "data.frame"))
+    repo <- tolower(match.arg(repo))
+    if (!inherits(db, "data.table")) setDT(db)
+    ind <- match(pkg, db[,Package])
+    if (is.na(ind)) stop("Package '", pkg, "' not known to package database.", call. = FALSE)
+
+    ## todo: check license and all that
+    D <- db[ind,]
+    if (debug) print(D)
+
+    lp <- tolower(pkg)
+    maint <- .getConfig("maintainer")
+    dhcompat <- .getConfig("debhelper_compat")
+    distribution <- .getConfig("distribution")
+
+    rel <- paste0("r-", repo, "-", lp)
+    ver <- paste0(D[,Version], "-1.obs.1")
+    date <- system("date -R", intern=TRUE)
+    con <- file("debian.changelog", "wt")
+    cat(rel, " (", ver, ") ", distribution, "; urgency=medium\n\n",
+        "  * OBS build\n\n",
+        " -- ", maint, "  ", date, "\n", sep="", file=con)
+    close(con)
+}
+
+#writeRules <- function(pkg, db) {
+writeRules <- function() {
+    ## if (missing(db)) db <- .pkgenv[["db"]]
+    ## stopifnot(`db must be data.frame`=inherits(db, "data.frame"))
+    ## if (!inherits(db, "data.table")) setDT(db)
+    ## ind <- match(pkg, db[,Package])
+    ## if (is.na(ind)) stop("Package '", pkg, "' not known to package database.", call. = FALSE)
+
+    con <- file("debian.rules", "wt")
+    cat("#!/usr/bin/make -f\n",
+        "\n",
+        "%:\n",
+        "\tdh $@ --buildsystem R\n",
+        sep="", file=con)
+    close(con)
+}
+
+writeFiles <- function(pkg, repo=c("CRAN", "Bioc")) {
+    stopifnot(`missing db`=is.finite(match("db", names(.pkgenv))))
+    repo <- match.arg(repo)
+
+    db <- setDT(.pkgenv[["db"]])
+    .downloadTarGz(pkg, db, repo)
+    writeControl(pkg, db=db, repo=repo)
+    writeChangelog(pkg, db=db, repo=repo)
+    writeRules() #pkg, db=db)
+    writeDsc(pkg, db=db, repo=repo)
+}
+
+.getField <- function(pkg, field, db, repo=c("CRAN", "Bioc")) {
+    if (missing(db)) db <- .pkgenv[["db"]]
+    stopifnot(`db must be data.frame`=inherits(db, "data.frame"))
+    if (!inherits(db, "data.table")) setDT(db)
+    ind <- match(pkg, db[,Package])
+    if (any(is.na(ind))) stop("Package '", pkg, "' not known to package database.", call. = FALSE)
+    if (is.na(match(field, names(db)))) stop("Field '", field, "' not known to package database.", call. = FALSE)
+    db[ind, ..field][[1]]
+}
+
+getVersion <- function(pkg, db, repo=c("CRAN", "Bioc")) {
+    .getField(pkg, "Version", db, repo)
+}
+
+.downloadTarGz <- function(pkg, db, repo=c("CRAN", "Bioc"), debug=FALSE) {
+    if (missing(db)) db <- .pkgenv[["db"]]
+    stopifnot(`db must be data.frame`=inherits(db, "data.frame"))
+    repo <- tolower(match.arg(repo))
+    if (!inherits(db, "data.table")) setDT(db)
+    ind <- match(pkg, db[,Package])
+    if (is.na(ind)) stop("Package '", pkg, "' not known to package database.", call. = FALSE)
+
+    ver <- getVersion(pkg, db, repo)
+    dst <- paste0(pkg, "_", ver, ".tar.gz")
+    if (file.exists(dst))
+        return(invisible)
+    if (repo == "cran") {
+        if (.hasConfigField("optional_cran_mirror")) {
+            src <- file.path(.getConfig("optional_cran_mirror"), dst)
+            stopifnot(`source file expected but not found`=file.exists(src))
+            file.copy(src, dst, overwrite=TRUE, copy.date=TRUE)
+        } else {
+            download.packages(pkg, ".")
+        }
+    }
+}
+
+## full cycle:
+##   osc mkpac pkgname
+##   cd pkgname
+##   c2d::writeFiles(pkgname, repo="CRAN")
+##   osc add debian.* *.tar.gz *.dsc
+##   osc build --local-package
+##   osc commit -m'r-cran-pkgname version'
---
+++ R/init.R
@@ -0,0 +1,100 @@
+
+.pkgenv <- new.env(parent=emptyenv())
+
+debug <- FALSE
+
+.defaultConfigFile <- function() {
+    if (getRversion() >= "4.0.0") {
+        ## ~/.local/share/R/ + package
+        pkgdir <- tools::R_user_dir(packageName())
+        if (dir.exists(pkgdir)) {
+            fname <- file.path(pkgdir, "config.dcf")
+            if (file.exists(fname)) {
+                return(fname)
+            }
+        }
+    }
+    return("")
+}
+
+.defaultCRANDBFile <- function() {
+    if (getRversion() >= "4.0.0") {
+        ## ~/.local/share/R/ + package
+        pkgdir <- tools::R_user_dir(packageName())
+        if (dir.exists(pkgdir)) {
+            fname <- file.path(pkgdir, "crandb.rds")
+            if (file.exists(fname)) {
+                return(fname)
+            }
+        } else {
+            message("No package config dir")
+        }
+    }
+    return("")
+}
+
+.loadConfig <- function() {
+    if (is.na(match("config_file", names(.pkgenv)))) {
+        if (debug) cat("Reading config\n")
+        cfgfile <- .defaultConfigFile()
+        if (cfgfile != "") {
+            cfg <- read.dcf(cfgfile)
+            .pkgenv[["config_file"]] <- cfgfile
+            .pkgenv[["user"]] <- cfg[1, "user"]
+            .pkgenv[["maintainer"]] <- cfg[1, "maintainer"]
+            .pkgenv[["distribution"]] <- cfg[1, "distribution"]
+            .pkgenv[["debhelper_compat"]] <- cfg[1, "debhelper_compat"]
+            .pkgenv[["minimum_r_version"]] <- cfg[1, "minimum_r_version"]
+            .pkgenv[["debian_policy_version"]] <- cfg[1, "debian_policy_version"]
+            .pkgenv[["cache_age_hours_cran_db"]] <- cfg[1, "cache_age_hours_cran_db"]
+            if (is.finite(match("optional_cran_mirror", colnames(cfg)))) {
+                .pkgenv[["optional_cran_mirror"]] <- cfg[1, "optional_cran_mirror"]
+            }
+        } else {
+            message("No config file")
+            .pkgenv[["config_file"]] <- ""
+        }
+    } else {
+        if (debug) cat("Have config\n")
+    }
+}
+
+.checkSystem <- function() {
+    bins <- c("osc", "md5sum", "sha1sum", "sha256sum")
+    res <- Sys.which(bins)
+    if (any(res==""))
+        stop("Missing binaries for '", paste(names(res[res==""]), collapse=", "), "'.", call. = FALSE)
+    invisible()
+}
+
+.loadDB <- function() {
+    if (is.na(match("db", names(.pkgenv)))) {
+        if (debug) cat("Reading db\n")
+        dbfile <- .defaultCRANDBFile()
+        hrs <- .pkgenv[["cache_age_hours_cran_db"]]
+        if (file.exists(dbfile) &&
+            as.numeric(difftime(Sys.time(), file.info(dbfile)$ctime, units="hours")) < hrs) {
+            db <- readRDS(dbfile)
+        } else {
+            db <- tools::CRAN_package_db()
+            if (dbfile != "") {
+                saveRDS(db, dbfile)
+            }
+        }
+        .pkgenv[["db"]] <- db
+    } else {
+        if (debug) cat("Have db\n")
+    }
+}
+
+.onLoad <- function(libname, pkgname) {
+    .loadConfig()
+    .checkSystem()
+    .loadDB()
+}
+
+.onAttach <- function(libname, pkgname) {
+    .loadConfig()
+    .checkSystem()
+    .loadDB()
+}
