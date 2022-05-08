3b0723e2bd05631283374a4b5db9ef9a17eaf3c2
Dirk Eddelbuettel <edd@debian.org>
2022-05-07 19:12:19 -0500

    add jammy setup script, rename other to focal

---
+++ inst/scripts/add_cranapt_jammy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+## See README.md for details on these steps
+
+## First: update apt and get gpg-agent and key
+apt update -qq
+apt install --yes --no-install-recommends gpg-agent  	# to add the key
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A1489FE2AB99A21A
+
+## Second: add the repo
+echo "deb [arch=amd64] https://dirk.eddelbuettel.com/cranapt jammy main" > /etc/apt/sources.list.d/cranapt.list
+apt update
+
+## Third: ensure R 4.2.0 is used (could use CRAN too)
+echo "deb [arch=amd64] http://ppa.launchpad.net/edd/misc/ubuntu jammy main" > /etc/apt/sources.list.d/edd-misc.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 67C2D66C4B1D4339
+
+## Fourth: add pinning to ensure package sorting
+echo "Package: *" > /etc/apt/preferences.d/99cranapt
+echo "Pin: origin \"dirk.eddelbuettel.com\"" >> /etc/apt/preferences.d/99cranapt
+echo "Pin-Priority: 700"  >> /etc/apt/preferences.d/99cranapt
+
+## Fifth: install bspm and enable it
+Rscript -e 'install.packages("bspm")'
+RHOME=$(R RHOME)
+echo "suppressMessages(bspm::enable())" >> ${RHOME}/etc/Rprofile.site
+echo "options(bspm.sudo=TRUE)" >> ${RHOME}/etc/Rprofile.site
