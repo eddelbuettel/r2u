566630e9d300bc73174f96390dd9cc1c2b4c2e22
Dirk Eddelbuettel <edd@debian.org>
2022-05-09 09:01:48 -0500

    updated cranapt script for jammy

--- inst/scripts/add_cranapt_jammy.sh
+++ inst/scripts/add_cranapt_jammy.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 ## See README.md for details on these steps
+##
+## This script has been tested on a plain ubuntu:22.04 system having R
+##
+## Note that you need to run this as root
 
-## First: update apt and get gpg-agent and key
+## First: update apt and get wget to fetch keys
 apt update -qq
-apt install --yes --no-install-recommends gpg-agent  	# to add the key
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A1489FE2AB99A21A
+apt install --yes --no-install-recommends wget ca-certificates
 
-## Second: add the repo
+## Second: add the CRAN apt repo and key
+wget -q -O- https://eddelbuettel.github.io/r2u/assets/dirk_eddelbuettel_key.asc \
+    | tee -a /etc/apt/trusted.gpg.d/cranapt_key.asc
 echo "deb [arch=amd64] https://dirk.eddelbuettel.com/cranapt jammy main" > /etc/apt/sources.list.d/cranapt.list
-apt update
 
-## Third: ensure R 4.2.0 is used (could use CRAN too)
-echo "deb [arch=amd64] http://ppa.launchpad.net/edd/misc/ubuntu jammy main" > /etc/apt/sources.list.d/edd-misc.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 67C2D66C4B1D4339
+## Third: ensure R 4.2.0 is used (could use Launchpad source or edd PPA too)
+wget -q -O- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc \
+    | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+echo "deb [arch=amd64] https://cloud.r-project.org/bin/linux/ubuntu jammy-cran40/" > /etc/apt/sources.list.d/cran_r.list
 
 ## Fourth: add pinning to ensure package sorting
 echo "Package: *" > /etc/apt/preferences.d/99cranapt
@@ -21,7 +26,11 @@ echo "Pin: origin \"dirk.eddelbuettel.com\"" >> /etc/apt/preferences.d/99cranapt
 echo "Pin-Priority: 700"  >> /etc/apt/preferences.d/99cranapt
 
 ## Fifth: install bspm and enable it
+##
+## If needed (in bare container, say) install python tools for bspm and R itself
+#DEBIAN_FRONTEND=noninteractive apt install --yes --no-install-recommends python3-dbus python3-apt r-base-core
+##
 Rscript -e 'install.packages("bspm")'
-RHOME=$(R RHOME)
+export RHOME=$(R RHOME)
 echo "suppressMessages(bspm::enable())" >> ${RHOME}/etc/Rprofile.site
 echo "options(bspm.sudo=TRUE)" >> ${RHOME}/etc/Rprofile.site
