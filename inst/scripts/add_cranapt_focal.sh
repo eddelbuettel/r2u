619c3d1677f86dde01543bad891cec44959f634b
Dirk Eddelbuettel <edd@debian.org>
2023-02-20 16:47:46 -0600

    Edit and refresh focal and jammy scripts

--- inst/scripts/add_cranapt_focal.sh
+++ inst/scripts/add_cranapt_focal.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-## See README.md for details on these steps
+## See the README.md of 'r2u' for details on these steps
+##
+## This script has been tested on a plain and minimal ubuntu:20.04
+##
+## On a well-connected machine this script should take well under one minute
+##
+## Note that you need to run this as root
 
-## First: update apt and get gpg-agent and key
-apt update -qq
-apt install --yes --no-install-recommends gpg-agent  	# to add the key
+## First: update apt and get keys
+apt update -qq && apt install --yes --no-install-recommends gpg-agent gnupg ca-certificates
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A1489FE2AB99A21A
 
-## Second: add the repo -- here we now use the mirror
-##echo "deb [arch=amd64] https://dirk.eddelbuettel.com/cranapt focal main" > /etc/apt/sources.list.d/cranapt.list
+## Second: add the repo -- here we use the well-connected mirror
 echo "deb [arch=amd64] https://r2u.stat.illinois.edu/ubuntu focal main" > /etc/apt/sources.list.d/cranapt.list
-apt update
 
 ## Third: ensure current R is used
-echo "deb [arch=amd64] https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/" > /etc/apt/sources.list.d/edd-misc.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 67C2D66C4B1D4339
+echo "deb [arch=amd64] https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/" > /etc/apt/sources.list.d/cran-r.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 67C2D66C4B1D4339 51716619E084DAB9
+apt update -qq
+DEBIAN_FRONTEND=noninteractive apt install --yes --no-install-recommends r-base-core
 
 ## Fourth: add pinning to ensure package sorting
 echo "Package: *" > /etc/apt/preferences.d/99cranapt
@@ -24,10 +29,9 @@ echo "Pin-Priority: 700"  >> /etc/apt/preferences.d/99cranapt
 
 ## Fifth: install bspm (and its Python requirements) and enable it
 ## If needed (in bare container, say) install python tools for bspm and R itself
-apt install python3-{dbus,gi,apt}
-## Then install bspm (as root) and enable it
+apt install --yes --no-install-recommends python3-{dbus,gi,apt}
+## Then install bspm (as root) and enable it, and enable a speed optimization
 Rscript -e 'install.packages("bspm")'
 RHOME=$(R RHOME)
 echo "suppressMessages(bspm::enable())" >> ${RHOME}/etc/Rprofile.site
-## Giving bspm sudo right used to be required but no longer is under current bspm versions
-#echo "options(bspm.sudo=TRUE)" >> ${RHOME}/etc/Rprofile.site
+echo "options(bspm.version.check=FALSE)" >> ${RHOME}/etc/Rprofile.site
--- inst/scripts/add_cranapt_jammy.sh
+++ inst/scripts/add_cranapt_jammy.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-## See README.md for details on these steps
+## See the README.md of 'r2u' for details on these steps
 ##
-## This script has been tested on a plain ubuntu:22.04 system having R
+## This script has been tested on a plain and minimal ubuntu:22.04
+##
+## On a well-connected machine this script should take well under one minute
 ##
 ## Note that you need to run this as root
 
-## First: update apt and get wget to fetch keys
-apt update -qq
-apt install --yes --no-install-recommends wget ca-certificates
-
-## Second: add the CRAN apt repo and key -- here we now use the mirror
+## First: update apt and get keys
+apt update -qq && apt install --yes --no-install-recommends wget ca-certificates gnupg
 wget -q -O- https://eddelbuettel.github.io/r2u/assets/dirk_eddelbuettel_key.asc \
-    | tee -a /etc/apt/trusted.gpg.d/cranapt_key.asc
-##echo "deb [arch=amd64] https://dirk.eddelbuettel.com/cranapt jammy main" > /etc/apt/sources.list.d/cranapt.list
+    | tee -a /etc/apt/trusted.gpg.d/cranapt_key.as
+
+## Second: add the repo -- here we use the well-connected mirror
 echo "deb [arch=amd64] https://r2u.stat.illinois.edu/ubuntu jammy main" > /etc/apt/sources.list.d/cranapt.list
 apt update
 
-## Third: ensure current R is used (could use Launchpad source or edd PPA too)
+## Third: ensure current R is used
 wget -q -O- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc \
     | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
 echo "deb [arch=amd64] https://cloud.r-project.org/bin/linux/ubuntu jammy-cran40/" > /etc/apt/sources.list.d/cran_r.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 67C2D66C4B1D4339 51716619E084DAB9
+apt update -qq
+DEBIAN_FRONTEND=noninteractive apt install --yes --no-install-recommends r-base-core
 
 ## Fourth: add pinning to ensure package sorting
 echo "Package: *" > /etc/apt/preferences.d/99cranapt
@@ -28,12 +31,11 @@ echo "Pin: release o=CRAN-Apt Project" >> /etc/apt/preferences.d/99cranapt
 echo "Pin: release l=CRAN-Apt Packages" >> /etc/apt/preferences.d/99cranapt
 echo "Pin-Priority: 700"  >> /etc/apt/preferences.d/99cranapt
 
-## Fifth: install bspm and enable it
+## Fifth: install bspm (and its Python requirements) and enable it
 ## If needed (in bare container, say) install python tools for bspm and R itself
-apt install python3-{dbus,gi,apt}
-## Then install bspm (as root) and enable it
+apt install --yes --no-install-recommends python3-{dbus,gi,apt}
+## Then install bspm (as root) and enable it, and enable a speed optimization
 Rscript -e 'install.packages("bspm")'
-export RHOME=$(R RHOME)
+RHOME=$(R RHOME)
 echo "suppressMessages(bspm::enable())" >> ${RHOME}/etc/Rprofile.site
-## Giving bspm sudo right used to be required but no longer is under current bspm versions
-#echo "options(bspm.sudo=TRUE)" >> ${RHOME}/etc/Rprofile.site
+echo "options(bspm.version.check=FALSE)" >> ${RHOME}/etc/Rprofile.site
