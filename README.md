c528af605ca83bd9635ee2947e5a253c363fb035
Dirk Eddelbuettel <edd@debian.org>
2024-05-12 10:37:24 -0500

    New release with full 24.04 support

--- README.md
+++ README.md
@@ -19,12 +19,11 @@
 - **Fast and well-connected mirror** at
   [r2u.stat.illinois.edu](https://r2u.stat.illinois.edu) on the [Internet2](https://internet2.edu/) 
 
-- **Complete coverage** with (currently, using 22.04) ~ 23175 CRAN packages (and 409 from
+- **Complete coverage** with (currently, using 22.04) ~ 23199 CRAN packages (and 411 from
   BioConductor) using **current versions**: We use R 4.4.0, and BioConductor 3.19 (on 24.04, and
   soon too on 22.04) and 3.18 (on 20.04).
 
-- Complete support for both **Ubuntu 20.04** ("focal") **and Ubuntu 22.04** ("jammy") **and Ubuntu
-  24.04** ("noble").
+- Complete support for **Ubuntu 20.04 ("focal")**,  **22.04 ("jammy")** and **24.04 ("noble")**.
 
 - Optional (but recommended) [bspm](https://cloud.r-project.org/package=bspm) use
   **automagically connects R functions like `install.packages()` to `apt`** for access to binaries 
@@ -83,8 +82,8 @@ So we now cover
     [scoring](https://bioconductor.org/packages/stats/bioc/bioc_pkg_scores.tab) BioConductor
     packages (also covering _e.g._ all BioConductor packages in the Debian and Ubuntu distributions)
 
-This currently results in 23270, 23182, 20624 binary packages from CRAN in "focal", "jammy", and
-"noble", respectively, and 408, 409, and 400 BioConductor packages, respectively, from the 3.19 and
+This currently results in 23286, 23199, 20744 binary packages from CRAN in "focal", "jammy", and
+"noble", respectively, and 412, 411, and 428 BioConductor packages, respectively, from the 3.19 and
 3.18 releases. (See this
 [FAQ](https://eddelbuettel.github.io/r2u/vignettes/FAQ/#why-does-it-have-more-packages-than-cran)
 about why this number is higher than CRAN, and variable between releases.)
---
+++ inst/scripts/add_cranapt_noble.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+## See the README.md of 'r2u' for details on these steps
+##
+## This script has been tested on a plain and minimal ubuntu:24.04
+##
+## On a well-connected machine this script should take well under one minute
+##
+## Note that you need to run this as root
+
+## First: update apt and get keys
+apt update -qq && apt install --yes --no-install-recommends wget ca-certificates gnupg
+wget -q -O- https://eddelbuettel.github.io/r2u/assets/dirk_eddelbuettel_key.asc \
+    | tee -a /etc/apt/trusted.gpg.d/cranapt_key.asc
+
+## Second: add the repo -- here we use the well-connected mirror
+echo "deb [arch=amd64] https://r2u.stat.illinois.edu/ubuntu noble main" > /etc/apt/sources.list.d/cranapt.list
+apt update
+
+## Third: ensure current R is used
+wget -q -O- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc \
+    | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+echo "deb [arch=amd64] https://cloud.r-project.org/bin/linux/ubuntu noble-cran40/" > /etc/apt/sources.list.d/cran_r.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 67C2D66C4B1D4339 51716619E084DAB9
+apt update -qq
+DEBIAN_FRONTEND=noninteractive apt install --yes --no-install-recommends r-base-core
+
+## Fourth: add pinning to ensure package sorting
+echo "Package: *" > /etc/apt/preferences.d/99cranapt
+echo "Pin: release o=CRAN-Apt Project" >> /etc/apt/preferences.d/99cranapt
+echo "Pin: release l=CRAN-Apt Packages" >> /etc/apt/preferences.d/99cranapt
+echo "Pin-Priority: 700"  >> /etc/apt/preferences.d/99cranapt
+
+## Fifth: install bspm (and its Python requirements) and enable it
+## If needed (in bare container, say) install python tools for bspm and R itself
+apt install --yes --no-install-recommends python3-{dbus,gi,apt} make
+## Then install bspm (as root) and enable it, and enable a speed optimization
+Rscript -e 'install.packages("bspm")'
+RHOME=$(R RHOME)
+echo "suppressMessages(bspm::enable())" >> ${RHOME}/etc/Rprofile.site
+echo "options(bspm.version.check=FALSE)" >> ${RHOME}/etc/Rprofile.site
