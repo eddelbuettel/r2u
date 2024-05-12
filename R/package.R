561ee9d236032d03878c70bce319c5bedcac72f0
Dirk Eddelbuettel <edd@debian.org>
2024-05-12 08:46:36 -0500

    Correct a sprintf call

--- R/package.R
+++ R/package.R
@@ -155,7 +155,7 @@ buildPackage <- function(pkg, tgt, debug=FALSE, verbose=FALSE, force=FALSE, xvfb
     pkgname <- paste0("r-", tolower(effrepo), "-", tolower(pkg)) 			# aka r-cran-namehere
     cand <- paste0(pkgname, "_", ver)
     if (is.finite(match(cand, builds[, pkgver])) && isFALSE(force) && suffix==".1") {
-        if (verbose) cat(blue(sprintf("%-22s %6s %-11s %-11s", pkg, ver, aver))) 		# start console log with pkg
+        if (verbose) cat(blue(sprintf("%-22s %-11s %-11s", pkg, aver, ver))) 		# start console log with pkg
         if (verbose) cat(green("[already built - skipping]\n"))
         return(invisible())
     }
